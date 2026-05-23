1880325a9c39a21b9a02faae997763b62f4c66ee
gokul-gituser <gokuljayanjb99@outlook.com>
2026-05-24 02:41:06 +0530

    import error

--- travel_assistant/faiss_store.py
+++ travel_assistant/faiss_store.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 import json
-from chunking import chunk_text
+from .chunking import chunk_text
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
